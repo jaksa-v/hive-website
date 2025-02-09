24b12f4fe1e46cc43a4a12794a6e842f2355600d
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2025-02-09 18:27:54 +0100

    make some modifications

--- src/components/MobileNavigation.jsx
+++ src/components/MobileNavigation.jsx
@@ -3,9 +3,13 @@ import menuIcon from "../assets/menu-icon.png";
 
 export default function MobileNavigation() {
   return (
-    <div className="fixed top-0 left-0 right-0 z-50 text-white py-4 px-[30px] flex justify-between bg-[#2B2B2B]/80 backdrop-blur-sm">
-      <img className="h-6 w-6" src={hiveLogo.src} alt="Hive logo" />
-      <img className="h-6 w-6 cursor-pointer" src={menuIcon.src} alt="Menu icon" />
+    <div className="fixed top-0 left-0 right-0 z-50 text-white py-2 px-[30px] flex justify-between items-center bg-[#2B2B2B]/80 backdrop-blur-sm">
+      <img className="h-10 w-10" src={hiveLogo.src} alt="Hive logo" />
+      <img
+        className="h-8 w-8 cursor-pointer"
+        src={menuIcon.src}
+        alt="Menu icon"
+      />
     </div>
   );
 }
