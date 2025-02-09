bc100d3c670f5137444bef7920460c91b6edf14c
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2025-02-09 14:54:34 +0100

    add new font and skech out the card ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ["Work Sans Variable", ...defaultTheme.fontFamily.sans],
+        mono: ["Space Mono", ...defaultTheme.fontFamily.mono],
       },
     },
   },
