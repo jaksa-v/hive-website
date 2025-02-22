10581d8d59a200d9f3a3041dc00be3bbfb2ba4a7
Jaksa Vlahovic <jaksa.vlahovic1@gmail.com>
2025-02-22 10:55:55 +0100

    Add programs

--- src/components/ProgramCard.tsx
+++ src/components/ProgramCard.tsx
@@ -18,43 +18,46 @@ export default function ProgramCard({ item }: ProgramCardProps) {
 
   return (
     <div className="bg-[#1E1E1E] rounded-[18px] flex flex-col p-4 border border-1 border-[#02C9BF] cursor-pointer transition-all duration-300 hover:shadow-[inset_0_0_15px_rgba(2,201,191,0.3)]">
-      <div className="rounded-[18px] flex flex-col relative h-[306px]">
-        <img
-          className="absolute inset-0 w-full h-full object-cover rounded-[18px]"
-          src={cardImage.src}
-          alt={title}
-        />
-        <div className="z-10 absolute w-full bottom-0 bg-[#3B3B3B] rounded-b-[18px] pt-2 pb-3 px-3">
-          <span className="text-white font-semibold">{title}</span>
-          <div className="flex gap-x-2 mt-2 font-mono">
-            <span className="text-xs text-[#858584]">
-              Broj učesnika: <span className="text-white">{participants}</span>
-            </span>
-            <span className="text-xs text-[#858584]">
-              Trajanje: <span className="text-white">{duration}</span>
-            </span>
-          </div>
-        </div>
-      </div>
-      <div className="flex gap-x-[14px] mt-4">
-        {activities.slice(0, 3).map((activity: Activity) => (
-          <div
-            key={activity.title}
-            className="rounded-[14px] flex flex-col relative h-[92px] flex-1"
-          >
-            <img
-              className="absolute inset-0 w-full h-full object-cover rounded-[14px]"
-              src={cardImage.src}
-              alt={activity.title}
-            />
-            <div className="z-10 absolute w-full bottom-0 bg-[#3B3B3B] rounded-b-[14px] px-2 py-1 flex justify-center">
-              <span className="text-xs font-medium text-white">
-                {activity.title}
+      <a href="/programs">
+        <div className="rounded-[18px] flex flex-col relative h-[306px]">
+          <img
+            className="absolute inset-0 w-full h-full object-cover rounded-[18px]"
+            src={cardImage.src}
+            alt={title}
+          />
+          <div className="z-10 absolute w-full bottom-0 bg-[#3B3B3B] rounded-b-[18px] pt-2 pb-3 px-3">
+            <span className="text-white font-semibold">{title}</span>
+            <div className="flex gap-x-2 mt-2 font-mono">
+              <span className="text-xs text-[#858584]">
+                Broj učesnika:{" "}
+                <span className="text-white">{participants}</span>
+              </span>
+              <span className="text-xs text-[#858584]">
+                Trajanje: <span className="text-white">{duration}</span>
               </span>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+        <div className="flex gap-x-[14px] mt-4">
+          {activities.slice(0, 3).map((activity: Activity) => (
+            <div
+              key={activity.title}
+              className="rounded-[14px] flex flex-col relative h-[92px] flex-1"
+            >
+              <img
+                className="absolute inset-0 w-full h-full object-cover rounded-[14px]"
+                src={cardImage.src}
+                alt={activity.title}
+              />
+              <div className="z-10 absolute w-full bottom-0 bg-[#3B3B3B] rounded-b-[14px] px-2 py-1 flex justify-center">
+                <span className="text-xs font-medium text-white">
+                  {activity.title}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </a>
     </div>
   );
 }
